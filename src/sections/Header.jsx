fd0a3363b39b0a3812d5a682c62031c70edffa96
Brandon <chitangb.chin@gmail.com>
2022-12-04 22:44:41 -0500

    Added additional profile description and 404 route handling

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -6,10 +6,9 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '/', current: false },
-  { name: 'About', href: '/About', current: false },
-  { name: 'Projects', href: '/Projects', current: false },
-  { name: 'Calendar', href: '/Calendar', current: false },
+  { name: 'Home', href: '/', current: false },
+  { name: 'Schedule a Call/Meeting', href: '/Calendar', current: false },
+  { name: 'Sandbox', href: '/Sandbox', current: false },
 ]
 
 function classNames(...classes) {
@@ -52,8 +51,8 @@ export default function Home() {
     <Disclosure as="nav">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+          <div className="mx-auto max-w-7xl">
+            <div className="relative flex h-16 right-3 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -65,7 +64,7 @@ export default function Home() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-1 sm:items-stretch sm:justify-start">
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
--- src/sections/HomeBody.jsx
+++ src/sections/HomeBody.jsx
@@ -1,19 +1,58 @@
-import Button from "../components/Button"
+import Image from 'next/image'
+import profilePic from '../public/me.png'
 
 const Home = () => {
 
 
   return (
     <div>
+      
     <section className="mt-16">
+      <Image src={profilePic} alt="ProfileImage" className="object-contain w-40 h-40 rounded-full"/>
         <h1 className="text-7xl font-bold dark:text-black-900">Brandon Chin</h1>
-        <h3 className="text-4xl my-3">Software Engineer</h3>
+        <h3 className="text-4xl my-5">Software Engineer</h3>
         <p className="mb-8">
-        My Goal is to help Developers while having fun in my sandbox
         </p>
     </section>
+    
+    <section className="mt-20">
+        <h1 className="text-4xl dark:text-white">Experience</h1>
+       
+    </section>
+
     <section className="mt-16">
-        <h1 className="text-7xl dark:text-purple-600 font-bold">Projects</h1>
+        <h1 className="text-2xl dark:text-white font-bold">Lighthouse | OSLabs</h1>
+        <h3>Co-Creator</h3>
+    </section>
+
+    <section className="mt-16">
+        <h1 className="text-2xl dark:text-white font-bold">JPI Healthcare Solutions</h1>
+        <h3>Regional Technical Sales Manager</h3>
+    </section>
+
+    <section className="mt-16">
+        {/* <h1 className="text-2xl dark:text-white font-bold">JPI Healthcare Solutions</h1> */}
+        <h3>Technical Systems Trainer</h3>
+    </section>
+
+    <section className="mt-20">
+        <h1 className="text-4xl dark:text-white">Open Source Projects</h1>
+       
+    </section>
+
+    <section className="mt-16">
+        <h1 className="text-2xl dark:text-white font-bold">VOT</h1>
+        <h3>Co-Creator</h3>
+    </section>
+
+    <section className="mt-16">
+        <h1 className="text-2xl dark:text-white font-bold">HomeGrub</h1>
+        <h3>Co-Creator</h3>
+    </section>
+
+    <section className="mt-16">
+        <h1 className="text-2xl dark:text-white font-bold">Green Planet</h1>
+        <h3>Co-Creator</h3>
     </section>
     </div>
   )
