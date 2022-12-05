5517ef253902de8451a2786a3266e1b6885a2efb
Brandon <chitangb.chin@gmail.com>
2022-12-04 20:43:36 -0500

    Fixed ESLINT error of React/no-unescaped-entities

--- src/sections/HomeBody.jsx
+++ src/sections/HomeBody.jsx
@@ -5,7 +5,7 @@ const Home = () => {
 
   return (
     <section className="mt-16">
-        <h1 className="text-7xl font-bold">Hi I'm <span className="dark:text-purple-600">Brandon</span></h1>
+        <h1 className="text-7xl font-bold">Hi I&apos;m <span className="dark:text-purple-600">Brandon</span></h1>
         <h3 className="text-4xl my-3">Software Engineer</h3>
         <p className="text-gray-700 mb-8">
         Helping developers build a faster web. Teaching about web development, serverless, and React / Next.js.
