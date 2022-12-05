da6fb2c2b7401d806dbaa1fc62fc462896bf42be
Brandon <chitangb.chin@gmail.com>
2022-12-05 01:17:03 -0500

    Cypress was tested

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="py-6 text-center text-gray-500 text-sm">
-        <span className="text-gray-900 font-bold text-lg mr-2 dark:text-gray-100">Brandon</span>&copy; {new Date().getFullYear()} Feel free to Copy~
+        <span className="text-gray-900 font-bold text-lg mr-2 dark:text-gray-100">Brandon</span>&copy; {new Date().getFullYear()} This Page was tested by Cypress
     </footer>
   )
 }
