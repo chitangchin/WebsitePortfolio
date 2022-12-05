7ede624219e03e6caba9db57188a6a1ecde2c707
Brandon <chitangb.chin@gmail.com>
2022-12-05 01:11:05 -0500

    Added Route Tests

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,18 +1,7 @@
-import YouTube from "react-youtube";
-
 const NotFound = () => {
-  const opts = {
-    height: "390",
-    width: "640",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
-  };
   return (
     <div className="text-5xl">
       WHAT ARE YOU DOING?? THAT PAGE DOESNT EXIST???
-      <YouTube videoId="dQw4w9WgXcQ" opts={opts} />
     </div>
   );
 };
