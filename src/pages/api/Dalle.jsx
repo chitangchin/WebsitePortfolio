c8e7ac9f7d5392881129330362a9c54416b1ed94
chitangchin <chitangb.chin@gmail.com>
2022-12-07 02:48:50 -0500

    put OpenAI key in env file

--- src/pages/api/Dalle.jsx
+++ src/pages/api/Dalle.jsx
@@ -1,29 +1,6 @@
-// import { Configuration, OpenAIApi } from "openai";
-
-// const config = new Configuration({  
-//     apiKey: "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",
-// });
-
-// const openai = new OpenAIApi(config);
-
-// const prompt = "write what you want to generate here";
-
-// export default async function handler(req, res) {
-//     // const response = await openai.createImage({
-//     //     prompt,
-//     //     n: 1,
-//     //     size: "1024x1024",
-//     // });
-    
-//     // const image = response.data.data[0].url;
-//     // console.log(image)
-
-//     res.status(200).json({image:{url:"asdadafadad"}})
-// }
-
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 export default async function handler(req, res) {
