6bd041284b869616579b52569dc514480170c2b4
chitangchin <chitangb.chin@gmail.com>
2022-12-11 13:20:33 -0500

    Added HoverEffects

--- src/components/LogoButtons.jsx
+++ src/components/LogoButtons.jsx
@@ -4,10 +4,10 @@ const logoButtons = () => {
     return (
         <div className="flex flex-row space-x-12 ">
             <a href="https://github.com/chitangchin">
-            <button className="bg-github-black dark:bg-github-white h-24 w-24 bg-contain"></button>
+            <button className="transition duration-300 ease-in-out hover:scale-110 bg-github-black dark:bg-github-white h-24 w-24 bg-contain"></button>
             </a>
             <a href="https://www.linkedin.com/in/chitangchin/">
-            <button className=" bg-linkedin h-24 w-24 bg-cover"></button>
+            <button className="transition duration-300 ease-in-out hover:scale-110 bg-linkedin h-24 w-24 bg-cover"></button>
             </a>
         </div>
     )
--- src/sections/HomeBody.jsx
+++ src/sections/HomeBody.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import LogoButtons from "../components/logoButtons"
+import LogoButtons from "../components/LogoButtons"
 
 const Home = () => {
   return (
