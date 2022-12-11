97f7f9f9848fb3c8c7ea730929e420393b011987
chitangchin <chitangb.chin@gmail.com>
2022-12-11 13:16:32 -0500

    Added Github and linkedin Buttons

--- src/components/LogoButtons.jsx
+++ src/components/LogoButtons.jsx
@@ -2,9 +2,13 @@ import Image from "next/image"
 
 const logoButtons = () => {
     return (
-        <div className="flex flex-row space-x-12">
-            <h1 className=" bg-github-black dark:bg-github-white h-24 w-24 bg-contain "></h1>
-            <h1 className=" bg-linkedin h-24 w-24 bg-cover"></h1>
+        <div className="flex flex-row space-x-12 ">
+            <a href="https://github.com/chitangchin">
+            <button className="bg-github-black dark:bg-github-white h-24 w-24 bg-contain"></button>
+            </a>
+            <a href="https://www.linkedin.com/in/chitangchin/">
+            <button className=" bg-linkedin h-24 w-24 bg-cover"></button>
+            </a>
         </div>
     )
 }
